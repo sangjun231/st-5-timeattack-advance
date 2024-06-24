56c39454f307481e8621f8da58181ae06f477393
sangjunShin <987654321123sin@gmail.com>
2024-06-24 16:42:46 +0900

    2번 제출

--- src/api/todos.js
+++ src/api/todos.js
@@ -3,3 +3,14 @@ import axios from "axios";
 export const todoApi = axios.create({
   baseURL: "http://localhost:4000",
 });
+
+export const updateTodo = async (id, currentLiked) => {
+  try {
+    const response = await todoApi.patch(`/todos/${id}`, {
+      liked: !currentLiked,
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,7 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import { todoApi } from "../api/todos";
+import { todoApi, updateTodo } from "../api/todos";
 
 export default function TodoList() {
   const navigate = useNavigate();
@@ -20,24 +20,42 @@ export default function TodoList() {
 
   // TODO: 아래 handleLike 로 구현되어 있는 부분을 useMutation 으로 리팩터링 해보세요. 모든 기능은 동일하게 동작해야 합니다.
   const queryClient = useQueryClient();
-  const handleLike = async (id, currentLiked) => {
-    const previousTodos = [...todos];
-    try {
+
+  const likeMutation = useMutation({
+    mutationFn: updateTodo,
+    onMutate: async ({ id, currentLiked }) => {
+      await queryClient.cancelQueries({ queryKey: ["todos"] });
+      queryClient.getQueryData(["todos"]);
       queryClient.setQueryData(["todos"], (prev) =>
         prev.map((todo) =>
-          todo.id === id ? { ...todo, liked: !todo.liked } : todo,
-        ),
+          todo.id === id ? { ...todo, liked: !todo.liked } : todo
+        )
       );
-      await todoApi.patch(`/todos/${id}`, {
-        liked: !currentLiked,
-      });
-    } catch (err) {
-      console.error(err);
-      queryClient.setQueryData(["todos"], previousTodos);
-    } finally {
-      refetch();
-    }
+    },
+  });
+
+  const handleLike = (id, currentLiked) => {
+    likeMutation.mutate({ id, currentLiked });
   };
+
+  // const handleLike = async (id, currentLiked) => {
+  //   const previousTodos = [...todos];
+  //   try {
+  //     queryClient.setQueryData(["todos"], (prev) =>
+  //       prev.map((todo) =>
+  //         todo.id === id ? { ...todo, liked: !todo.liked } : todo
+  //       )
+  //     );
+  //     await todoApi.patch(`/todos/${id}`, {
+  //       liked: !currentLiked,
+  //     });
+  //   } catch (err) {
+  //     console.error(err);
+  //     queryClient.setQueryData(["todos"], previousTodos);
+  //   } finally {
+  //     refetch();
+  //   }
+  // };
 
   if (isPending) {
     return <div style={{ fontSize: 36 }}>로딩중...</div>;
